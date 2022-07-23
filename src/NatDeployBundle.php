3b9093289bc52719dbcd2af6691976500f399939
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-07-23 22:56:28 +0200

    test without loading in main bundle class

--- src/NatDeployBundle.php
+++ src/NatDeployBundle.php
@@ -2,7 +2,6 @@
 
 namespace Nat\DeployBundle;
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NatDeployBundle extends Bundle
@@ -12,23 +11,4 @@ class NatDeployBundle extends Bundle
         return __DIR__;
     }
 
-    private static $containerInstance = null;
-
-    public function setContainer(\Symfony\Component\DependencyInjection\ContainerInterface $container = null)
-    {
-        parent::setContainer($container);
-        self::$containerInstance = $container;
-    }
-
-    public static function getContainer()
-    {
-        return self::$containerInstance;
-    }
-
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
-    {
-        // load an XML, PHP or Yaml file
-        $container->import('../config/services.yml');
-
-    }
 }
