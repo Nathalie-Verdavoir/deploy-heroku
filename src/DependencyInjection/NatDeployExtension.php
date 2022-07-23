8d5bce295edaa687203fe5c36346aa3bb0fd0812
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-07-23 17:33:31 +0200

    get extension back

---
+++ src/DependencyInjection/NatDeployExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Nat\DeployBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader ;
+
+class NatDeployExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../../config')
+        );
+        $loader->load('services.yml');
+    }
+}
