901b9ddcccebf365de97530c59e4343ae52578e3
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-07-23 16:07:27 +0200

    load container

--- src/NatDeployBundle.php
+++ src/NatDeployBundle.php
@@ -10,4 +10,17 @@ class NatDeployBundle extends Bundle
     {
         return __DIR__;
     }
+
+    private static $containerInstance = null;
+
+    public function setContainer(\Symfony\Component\DependencyInjection\ContainerInterface $container = null)
+    {
+        parent::setContainer($container);
+        self::$containerInstance = $container;
+    }
+
+    public static function getContainer()
+    {
+        return self::$containerInstance;
+    }
 }
